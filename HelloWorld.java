88ecd24eeb4652722f0deb1ffcb4ef253afcb1a0
Si Chen <quake0day@gmail.com>
2018-03-30 12:35:07 -0400

    OK

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,20 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
+		System.out.println("HI!!! World!!!!!");
 		System.out.println("Hello World!!!!!");
 	}
 }
