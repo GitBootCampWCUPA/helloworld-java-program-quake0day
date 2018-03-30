9963531de0a2570a683ced4477c3c94fbf0a99bf
Si Chen <quake0day@gmail.com>
2018-03-30 12:50:12 -0400

    made changes to main method

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,9 +13,5 @@ public class HelloWorld
 		System.out.println("Hello World!!!!!");
 		System.out.println("Hello World!!!!!");
 		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
-		System.out.println("Hello World!!!!!");
 	}
 }
